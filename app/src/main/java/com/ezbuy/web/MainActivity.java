03816f0f6d51bbc245604998ab6b11e9e86aa5f4
yutianran <yutianran@ezbuy.com>
2019-01-21 10:02:18 +0800

    add all file

---
+++ app/src/main/java/com/ezbuy/web/MainActivity.java
@@ -0,0 +1,82 @@
+package com.ezbuy.web;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        init();
+    }
+
+    private void init() {
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                if (title != null || !title.isEmpty()) {
+//                    tvTitle.setText(title);
+                }
+            }
+        });
+
+        WebSettings webSetting = webView.getSettings();
+        webSetting.setAllowFileAccess(true);
+        webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSetting.setSupportZoom(true);
+        webSetting.setBuiltInZoomControls(false);
+        webSetting.setUseWideViewPort(true);
+        webSetting.setSupportMultipleWindows(false);
+        // webSetting.setLoadWithOverviewMode(true);
+        webSetting.setAppCacheEnabled(true);
+        // webSetting.setDatabaseEnabled(true);
+        webSetting.setGeolocationEnabled(true);
+        webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
+        webSetting.setAppCachePath(getBaseActivity().getDir("appcache", 0).getPath());
+        webSetting.setDatabasePath(getBaseActivity().getDir("databases", 0).getPath());
+        webSetting.setGeolocationDatabasePath(getBaseActivity().getDir("geolocation", 0)
+                .getPath());
+        // webSetting.setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);
+        webSetting.setPluginState(WebSettings.PluginState.ON_DEMAND);
+
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setCacheMode(WebSettings.LOAD_NO_CACHE);//不设置缓存
+        webSetting.setJavaScriptEnabled(true);
+//        webView.loadUrl("file:///android_asset/web/index.html");
+//        webView.loadUrl("http://121.199.36.228:9090/index.html");
+        webView.loadUrl("http://121.199.36.228:8090/index.html");
+    }
+
+    private Context getBaseActivity() {
+        return this;
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();// 返回前一个页面
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+}
